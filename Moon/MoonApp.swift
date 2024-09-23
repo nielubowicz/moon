3ab70f0029e8cae8be53e19f359827e50d518614
Chris Nielubowicz <nielubowicz@gmail.com>
2024-09-23 19:43:07 -0400

    Initial Commit

---
+++ Moon/MoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonApp.swift
+//  Moon
+//
+//  Created by mac on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
